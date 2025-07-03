9b767dbf6f8b03ad9b8bc7e07cd42b8d8f1e6937
Heet2405 <heet.maulik@gmail.com>
2025-07-03 10:27:14 -0700

    first print statment

--- learn.py
+++ learn.py
@@ -1,1 +1,25 @@
-print(5)
+print (""" Wedding Poem by Whitney Hanson Poetry
+They say that sometimes
+love starts with a spark.
+
+And that might be true,
+but if I were to wish you a love,
+I wouldn't wish fire for you.
+
+You see, fire is powerful.
+It burns bright and then it's gone.
+It's beautiful and warm,
+but it doesn't last long.
+
+So instead of wishing you a love that burns,
+I wish you a love like a river twists and turns.
+It changes and it flows,
+It is powerful and free.
+But it consistently finds its way back to the sea.
+
+And so like the water,
+I hope your love is ever growing, ever changing.
+I hope your love is powerful and free,
+And may you always find each other,
+
+Like a river finds the sea.""")
